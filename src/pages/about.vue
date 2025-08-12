b687c08bbbaa6970bc91bde2ade7782c93b999ed
denisphamzero7 <pnhau2001@gmail.com>
2025-08-12 12:18:30 +0700

    update

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,3 +1,11 @@
+/* stylelint-disable @stylistic/block-opening-brace-space-before */
 <template>
-  <p>This is about page</p>
+  <p class="some-icon">This is about page</p>
+
+  <p class="size">13434</p>
 </template>
+
+<style lang="scss">
+/* stylelint-disable-next-line @stylistic/string-quotes */
+@use '@layouts/styles/mixins1';
+</style>
