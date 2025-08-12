402f9a1ee981852412ff20d9ec72f6946c32496f
denisphamzero7 <pnhau2001@gmail.com>
2025-08-12 10:48:20 +0700

    How to Create A New Page

---
+++ src/pages/[...404].vue
@@ -0,0 +1,19 @@
+<!-- eslint-disable vue/no-deprecated-router-link-tag-prop -->
+<script lang="ts" setup>
+import { definePage } from 'vue-router/auto'
+
+definePage({
+  meta: {
+    layout: 'blank',
+  },
+})
+</script>
+
+<template>
+  <div>
+    <p>
+      ohhh,bạn đã saiii ạ
+       <RouterLink tag="p" to="/">Go back home</RouterLink>
+    </p>
+  </div>
+</template>
